1bda38462addc3b993af30c7ca498e06def4bf90
xShadowxIQ <125368135+xShadowxIQ@users.noreply.github.com>
2026-09-19 20:47:33 +0300

    audit: fuzz framed wire message decoding

--- wire/audit_fuzz_test.go
+++ wire/audit_fuzz_test.go
@@ -2,7 +2,10 @@ package wire
 
 import (
 	"bytes"
+	"encoding/binary"
 	"testing"
+
+	"github.com/decred/dcrd/chaincfg/chainhash"
 )
 
 func FuzzAuditMsgTxDecode(f *testing.F) {
@@ -26,5 +29,33 @@ func FuzzAuditMsgBlockDecode(f *testing.F) {
 
 		var blockWire MsgBlock
 		_ = blockWire.BtcDecode(bytes.NewReader(data), ProtocolVersion)
+	})
+}
+
+func auditFramedMessage(command string, payload []byte) []byte {
+	var header [MessageHeaderSize]byte
+	binary.LittleEndian.PutUint32(header[0:4], uint32(MainNet))
+	copy(header[4:4+CommandSize], command)
+	binary.LittleEndian.PutUint32(header[4+CommandSize:8+CommandSize], uint32(len(payload)))
+	checksum := chainhash.HashH(payload)
+	copy(header[8+CommandSize:MessageHeaderSize], checksum[:4])
+	return append(header[:], payload...)
+}
+
+func FuzzAuditReadMessageNTx(f *testing.F) {
+	f.Add([]byte{0})
+	f.Add([]byte{1, 0, 0, 0})
+	f.Fuzz(func(t *testing.T, payload []byte) {
+		_, _, _, _ = ReadMessageN(bytes.NewReader(auditFramedMessage(CmdTx, payload)),
+			ProtocolVersion, MainNet)
+	})
+}
+
+func FuzzAuditReadMessageNBlock(f *testing.F) {
+	f.Add([]byte{0})
+	f.Add([]byte{1, 0, 0, 0})
+	f.Fuzz(func(t *testing.T, payload []byte) {
+		_, _, _, _ = ReadMessageN(bytes.NewReader(auditFramedMessage(CmdBlock, payload)),
+			ProtocolVersion, MainNet)
 	})
 }
